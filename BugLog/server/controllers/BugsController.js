e80ad8c7912125199b323927be971fc9640ad5a5
Timothy Kimble <timble417@gmail.com>
2021-08-13 15:46:19 -0600

    What was done in class together for setup

--- BugLog/server/controllers/BugsController.js
+++ BugLog/server/controllers/BugsController.js
@@ -1,29 +1,22 @@
 import BaseController from '../utils/BaseController'
 import { Auth0Provider } from '@bcwdev/auth0provider'
+import { bugsService } from '../services/BugsService'
 
-export class ValuesController extends BaseController {
+export class BugsController extends BaseController {
   constructor() {
-    super('api/values')
+    super('api/bugs')
     this.router
-      .get('', this.getAll)
       // NOTE: Beyond this point all routes require Authorization tokens (the user must be logged in)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.create)
   }
 
-  async getAll(req, res, next) {
-    try {
-      return res.send(['value1', 'value2'])
-    } catch (error) {
-      next(error)
-    }
-  }
-
   async create(req, res, next) {
     try {
-      // NOTE NEVER TRUST THE CLIENT TO ADD THE CREATOR ID
+      // Do Not Trust the CLient
       req.body.creatorId = req.userInfo.id
-      res.send(req.body)
+      const bug = await bugsService.create(req.body)
+      res.send(bug)
     } catch (error) {
       next(error)
     }
