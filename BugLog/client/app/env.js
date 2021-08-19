6d006c20d6594280ef1501090a99111d75a15aff
Timothy Kimble <timble417@gmail.com>
2021-08-19 08:54:48 -0600

    deleted server client

--- BugLog/client/app/env.js
+++
@@ -1,5 +0,0 @@
-export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = 'codeworksclassroom.auth0.com'
-export const audience = 'https://codeworksclassroom.com'
-export const clientId = 'pOXw2OGv1LsYi7LEBmDF04RLkXQvldml'
